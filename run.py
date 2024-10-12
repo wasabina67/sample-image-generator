268a454466d4e7fd888fc1124fd1b73ada81ba5f
wasabina67 <wasabina67@gmail.com>
2024-10-12 10:31:56 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
 def create_sample_image():
+    width, height = 800, 1600
+    color = (200, 200, 200)
+    img = Image.new("RGB", (width, height), color=color)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 80)
+
+    sample_text = "Sample Text"
     return
 
 
